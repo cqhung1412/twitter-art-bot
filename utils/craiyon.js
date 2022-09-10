806fc5448effd41df039f647e430d4169fbe000e
hungcq3 <hungcq3@vng.com.vn>
2022-09-11 03:55:04 +0700

    reduce image count

--- utils/craiyon.js
+++ utils/craiyon.js
@@ -7,7 +7,8 @@ const getImages = async (prompt) => {
   const result = await craiyon.generate({
     prompt,
   });
-  
+  result.images.length = 4; // Tweet must not have more than 4 mediaIds
+
   let bufferImages = [];
   result.images.forEach((image) => {
     bufferImages.push(image.asBuffer());
